78186e54fb5ed5345ef7ddbd09692e3ff61fb99d
Vojislav Grujic <vojislav.grujic@gmail.com>
2017-02-26 19:02:45 +0100

    Add 'prod/components' folders to the project.

--- components/modals/modals.js
+++ components/modals/modals.js
@@ -1,4 +1,54 @@
 (function() {
-	//logic
+
+  var modal = {
+
+    init: function() {
+      this.cacheDom();
+      this.addEvents();
+    },
+
+    cacheDom: function() {
+      this.modalTriggers = document.querySelectorAll('[data-modal-trigger]');
+      // this.modalContainers = document.querySelectorAll('.su_modal:not(.su_modal-simple)');
+      this.simpleModal = document.querySelector('.su_modal-simple');
+      this.imageModal = document.querySelector('.su_modal-image');
+      this.videoModal = document.querySelector('.su_modal-video');
+    },
+
+    addEvents: function() {
+      var i, len = this.modalTriggers && this.modalTriggers.length;
+
+      for ( i = 0; i < len; i++ ) {
+        this.modalTriggers[i].addEventListener('click', this.toggleModal.bind(this));
+      }
+
+    },
+
+    toggleModal: function(e) {
+      console.log('Modal');
+      var modalType = e.target.dataset.modalTrigger,
+          simpleModal = (modalType === 'simple') && this.simpleModal,
+          imageModal = (modalType === 'image') && this.imageModal,
+          videoModal = (modalType === 'video') && this.videoModal,
+          modalContent = e.target.dataset.info,
+          modalActive = e.target.classList.contains('su_modal-active'),
+          modalContentContainer;
+
+      if ( simpleModal ) {
+        modalContentContainer = simpleModal.querySelector('.su_modal-content');
+        if ( !modalActive ) {
+          modalContentContainer.textContent = modalContent;
+          simpleModal.classList.add('su_modal-active');
+        } else {
+          modalContentContainer.textContent = '';
+          simpleModal.classList.remove('su_modal-active');
+        }
+      }
+
+    }
+
+  }
+
+  modal.init();
 
 })();
