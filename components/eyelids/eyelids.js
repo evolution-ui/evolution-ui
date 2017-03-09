1a01bb457456a41852a29ae36e46aaafaadfbe7d
Vojislav Grujic <vojislav.grujic@gmail.com>
2017-03-09 05:27:02 +0100

    Update Eyelids to adjust width of the highlighted area.
    * Added Grunt tasks that copy the 'main.css' and 'main.js' from the 'dist' to the 'prod' folder.

--- components/eyelids/eyelids.js
+++ components/eyelids/eyelids.js
@@ -5,21 +5,35 @@
   var eyelids = {
 
     width: 180,
+    step: 10,
+    limitUp: 400,
+    limitDown: 50,
     enabled: false,
+    helperCreated: false,
+    key: '1',
+    modifier: 'ctrlKey',
+    helpItems: [
+      'Hold "Shift" and scroll mouse to resize the highlight area.',
+      'Don\'t take crap from anyone!'
+    ],
 
     init: function() {
       this.cacheDom();
+      this.setShortcut();
       this.notify();
       this.addEvent(this.bodyElement, 'keydown', this.toggle.bind(this));
-      this.addEvent(this.notification, 'mouseenter', this.notifyStop.bind(this));
+      this.addEvent(this.bodyElement, 'wheel', this.resize.bind(this));
       this.addEvent(this.notification, 'click', this.notify.bind(this));
     },
 
     cacheDom: function() {
       this.bodyElement = document.querySelector('body');
       this.notification = document.querySelector('.su_eyelids-notification');
+      this.shortcut = this.notification && this.notification.querySelector('code');
       this.top = document.createElement('div');
       this.bottom = document.createElement('div');
+      this.helper = document.createElement('div');
+      this.helper.classList.add('su_eyelids-helper');
       this.top.classList.add('su_eyelids-top');
       this.bottom.classList.add('su_eyelids-bottom');
     },
@@ -36,12 +50,26 @@
       } else return;
     },
 
-    notify: function() {
-      this.notification && this.notification.classList.add('su_active');
+    setShortcut: function() {
+      if ( this.shortcut ) {
+        this.shortcut.textContent = this.modifier + ' + ' + this.key;
+      }
     },
 
-    notifyStop: function() {
-      this.notification && this.notification.classList.remove('su_active');
+    notify: function() {
+      var notification = this.notification;
+      if ( notification ) {
+        if ( notification.classList.contains('su_active') ) {
+          notification.classList.remove('su_active');
+          window.clearTimeout(removeClass);
+        } else {
+          notification.classList.add('su_active');
+          removeClass = window.setTimeout(function() {
+            notification.classList.remove('su_active');
+            window.clearTimeout(removeClass);
+          }, 5000);
+        }
+      }
     },
 
     enable: function() {
@@ -50,12 +78,14 @@
       // this.top.style.height = 'calc(50vh - ' + this.width / 2 + 'px)';
       // this.bottom.style.top = 'calc(50vh + ' + this.width / 2 + 'px)';
       this.enabled = true;
+      this.addHelper();
     },
 
     disable: function() {
       this.top = this.bodyElement.removeChild(this.top);
       this.bottom = this.bodyElement.removeChild(this.bottom);
       this.enabled = false;
+      this.removeHelper();
     },
 
     follow: function(e) {
@@ -64,16 +94,58 @@
     },
 
     toggle: function(e) {
-      var key = e.key.toLowerCase(),
-          shift = e.shiftKey;
+      var key = e.key.toLowerCase();
 
-      if ( this.enabled && (key === 's' && shift || key === 'escape') ) {
+      // console.log(e.key);
+
+      if ( this.enabled && (key === this.key && e[this.modifier] || key === 'escape') ) {
+        e.preventDefault();
         this.disable();
         this.removeEvent(document, 'mousemove', this.follow.bind(this));
-      } else if ( key === 's' && shift ) {
+      } else if ( key === this.key && e[this.modifier] ) {
+        e.preventDefault();
         this.enable();
         this.addEvent(document, 'mousemove', this.follow.bind(this));
       }
+    },
+
+    resize: function(e) {
+      if ( this.enabled && e.shiftKey ) {
+        e.preventDefault();
+        if ( e.deltaY > 0 ) {
+          this.width += this.width < this.limitUp && this.step;
+        } else {
+          this.width -= this.width > this.limitDown && this.step;
+        }
+        this.follow(e);
+      }
+    },
+
+    addHelper: function() {
+      // if helper does not exist, i.e. the first run of eyelids, then create its content
+      if ( !this.helperCreated ) {
+        var note = document.createElement('p'),
+            list = document.createElement('ul'),
+            li,
+            i, len = this.helpItems.length;
+
+        for ( i = 0; i < len; i++ ) {
+          li = document.createElement('li');
+          li.textContent = this.helpItems[i];
+          list.appendChild(li);
+        }
+        note.textContent = 'Eyelids help:';
+        this.helper.appendChild(note);
+        this.helper.appendChild(list);
+        this.helperCreated = true;
+      }
+
+      // append helper to the body, this happens no matter if it's the first run or not
+      this.bodyElement.appendChild(this.helper);
+    },
+
+    removeHelper: function() {
+      this.helper = this.bodyElement.removeChild(this.helper);
     }
 
   };
