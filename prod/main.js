9d4ed78dec8e0ff5dad391dd810ec764af22c532
Brian Hernandez <brianhernandez@users.noreply.github.com>
2017-03-14 17:09:23 -0400

    Improved .su_full-width class by adding display: block to it.

--- prod/main.js
+++ prod/main.js
@@ -992,7 +992,28 @@
 /** END STICKY COMPONENT **/
 
 (function() {
-	//logic
+
+  var tabs = document.querySelectorAll(".tabs-titles li"); //grab tabs
+  var tabsArray = [].slice.call(tabs);
+  var contents = document.querySelectorAll('.tabs-contents li'); //grab contents
+
+  console.log(tabs);
+
+  for(i=0; i <= tabs.length; i++) {
+    tabs[i].addEventListener('click', function(callback){
+      console.dir(tabsArray.indexOf(this));
+
+      for (content in contents) {
+        contents[content].style.display = "none";
+      }
+
+      this.parentElement.querySelector(".current").className = ""; // Remove class current from other tabs
+      this.className += "current"; // Add class current to this tab
+
+      contents[tabsArray.indexOf(this)].style.display = "block"; //show tab content that matches tab title index
+
+    });
+  }
 
 })();
 
