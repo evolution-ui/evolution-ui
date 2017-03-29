f1625dd5201386e2bad0a2d1d2ca778081b1ea3b
Brian Hernandez <brianhernandez@users.noreply.github.com>
2017-03-29 03:54:04 -0400

    Added prismjs JS and CSS files for prism support and export/import prismjs to app.js.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,10 +3,11 @@ import crumble from './evolution/crumble'
 import dotNavigation from './evolution/dot_navigation'
 import eyelids from './evolution/eyelids'
 import cssAnimations from './standard/css_animations'
-
+import prismjs from './standard/prism'
 
 offCanvasNavigation();
 crumble();
 dotNavigation();
 eyelids();
 cssAnimations();
+prismjs();
