20da94d2ff6ebfc884c925af871dfc0ba344262f
Gabriele Romeo <gabriele.romeo@gmail.com>
2017-04-02 12:14:08 +0200

    Improve organization

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,30 @@
-import offCanvasNavigation from './standard/off_canvas_navigation'
+/************************************************************************
+ *
+ *                      EVOLUTION COMPONENTS
+ *
+ ***********************************************************************/
+
+
 import crumble from './evolution/crumble'
 import dotNavigation from './evolution/dot_navigation'
 import eyelids from './evolution/eyelids'
 import fadeLinesOnScroll from './evolution/fade_lines_on_scroll'
+
+
+/************************************************************************
+ *
+ *                      STANDARD COMPONENTS
+ *
+ ***********************************************************************/
+
+import offCanvasNavigation from './standard/off_canvas_navigation'
 import cssAnimations from './standard/css_animations'
 import codeMarkup from './standard/code_markup'
+import stdForms from './standard/forms'
+
+
+
+
 
 offCanvasNavigation();
 crumble();
@@ -13,3 +33,4 @@ eyelids();
 fadeLinesOnScroll();
 cssAnimations();
 codeMarkup();
+stdForms();
