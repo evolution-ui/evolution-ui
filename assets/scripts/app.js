d9b6060506c97606d195449f8faf7b91062353a7
Imanuel Gittens <gittensimanuel@gmail.com>
2017-04-02 13:49:03 -0400

    Complete Evolution UI demo component.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,6 +9,7 @@ import crumble from './evolution/crumble'
 import dotNavigation from './evolution/dot_navigation'
 import eyelids from './evolution/eyelids'
 import fadeLinesOnScroll from './evolution/fade_lines_on_scroll'
+import demoComponent from './evolution/demo-component'
 
 
 /************************************************************************
@@ -34,3 +35,4 @@ fadeLinesOnScroll();
 cssAnimations();
 codeMarkup();
 stdForms();
+demoComponent();
