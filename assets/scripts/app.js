ecc143cd88742aa9465b8ad5c458343fc5fc299e
Vojislav Grujic <vojislav.grujic@gmail.com>
2017-04-03 18:25:35 +0200

    Fix merge issue

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,11 +9,8 @@ import crumble from './evolution/crumble'
 import dotNavigation from './evolution/dot_navigation'
 import eyelids from './evolution/eyelids'
 import fadeLinesOnScroll from './evolution/fade_lines_on_scroll'
-<<<<<<< HEAD
 import demoComponent from './evolution/demo-component'
-=======
 import readabilityPanel from './evolution/readability_improvement_panel';
->>>>>>> nunez
 
 
 /************************************************************************
