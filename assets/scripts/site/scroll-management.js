522a3754c49e19ca1ae4e6d0f6b2c0b07306118a
Vojislav Grujic <vojislav.grujic@gmail.com>
2017-04-05 02:14:54 +0200

    Fix code issues.
    * Id names in html and css didn't match, so they're renamed to match.
    * Changed some styles to make the component easier to understand and include into other html content.
    * Added variable checks into JS files 'site/menu-highlight.js' and 'site/scroll-management.js'

--- assets/scripts/site/scroll-management.js
+++ assets/scripts/site/scroll-management.js
@@ -6,7 +6,7 @@ import scrollSpy from './scrollspy'
 export default () => {
   const siteMain = document.querySelector('.site-main')
   const sideMenu = document.querySelector('.js-offcanvas-target')
-  let sideMenuWidth = sideMenu.offsetWidth
+  let sideMenuWidth = sideMenu && sideMenu.offsetWidth
   const sectionLinks = document.querySelectorAll('.site-sidebar-link')
   const sections = document.querySelectorAll('.site-section')
   const fixedHeader = document.querySelector('.evo_c-scrollspy')
@@ -24,13 +24,15 @@ export default () => {
   const sideMenuStickyHandler = throttle(() => {
     const topOffset = siteMain.offsetTop
 
-    if (window.pageYOffset > topOffset) {
-      sideMenu.style.width = sideMenuWidth + 'px'
-      sideMenu.classList.add('is-sticky')
-    } else {
-      sideMenu.removeAttribute('style')
-      sideMenu.classList.remove('is-sticky')
-      sideMenuWidth = sideMenu.offsetWidth
+    if ( sideMenu ) {
+      if (window.pageYOffset > topOffset) {
+        sideMenu.style.width = sideMenuWidth + 'px'
+        sideMenu.classList.add('is-sticky')
+      } else {
+        sideMenu.removeAttribute('style')
+        sideMenu.classList.remove('is-sticky')
+        sideMenuWidth = sideMenu.offsetWidth
+      }
     }
   }, 100)
 
