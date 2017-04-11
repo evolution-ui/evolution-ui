14c4a78d770f8e205c08843e283ba1475e714089
strtw <souporstu@gmail.com>
2017-04-11 12:39:46 -0700

    updated component

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -32,6 +32,7 @@ import curtain from './evolution/curtain'
 import text2speech from './evolution/text2speech'
 import svg_pagination from './evolution/svg_pagination'
 import divTable from './evolution/div_table'
+import searchWithLongEdit from './evolution/search_with_long_edit'
 
 /************************************************************************
  *
@@ -88,7 +89,8 @@ documentReady(
   curtain,
   text2speech,
   svg_pagination,
-  divTable
+  divTable,
+  searchWithLongEdit
 )
 
 singleInputForm();
