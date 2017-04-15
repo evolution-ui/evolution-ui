0c9363d18ea0e0544ac34d0e7c6a994068870f3d
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2017-04-14 20:53:31 -0500

    made changes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -25,6 +25,7 @@ import curtain from './evolution/curtain'
 import text2speech from './evolution/text2speech'
 import svg_pagination from './evolution/svg_pagination'
 import divTable from './evolution/div_table'
+import carouselDrop from './evolution/carousel_with_dropping_circle_nav'
 
 /************************************************************************
  *
