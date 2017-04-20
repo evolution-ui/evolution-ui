a8c15ff6b121ef88e949cd2aa406167b09457cba
Brian Hernandez <brianhernandez@users.noreply.github.com>
2017-04-20 15:36:09 -0400

    Pretty much completed Fade Lines On Scroll component, finally omg.

--- assets/scripts/evolution/fade_lines_on_scroll.js
+++ assets/scripts/evolution/fade_lines_on_scroll.js
@@ -8,31 +8,30 @@ export default function() {
     fadedTextBlocks[i].innerHTML = wrappedWordsInSpanTags;
 
     for (var j = 0; j < fadedTextBlocks[i].childNodes.length; j++) {
-      var temporaryScatterClass = 'class="evo_h-fade-lines-' + j + '"';
-      var randomTopDistValue = Math.floor(Math.random() * 200) + 80;
+      var randomTopDistValue = Math.random() * 300 + 80;
+      // var randomRotationValue = Math.random() * 360;
       if (fadedTextBlocks[i].childNodes[j].tagName === "SPAN") {
-        fadedTextBlocks[i].childNodes[j].classList.add(temporaryScatterClass);
         fadedTextBlocks[i].childNodes[j].style.position = "relative";
         fadedTextBlocks[i].childNodes[j].style.top = randomTopDistValue + "px";
-        // fadedTextBlocks[i].childNodes[j].style.transform = "translateY(" + randomTopDistValue + "px)";
         fadedTextBlocks[i].childNodes[j].style.opacity = 0;
-        fadedTextBlocks[i].childNodes[j].style.transition = 'all 3s';
-        fadedTextBlocks[i].childNodes[j].style.transition = 'top 3s, opacity 3s';
+        // fadedTextBlocks[i].childNodes[j].style.transform = "rotate(" + randomRotationValue + "deg)";
+        console.log(fadedTextBlocks[i].childNodes[j].style.transform);
+        fadedTextBlocks[i].childNodes[j].style.transition = 'top 3s, opacity 3s, transform 3s';
       }
     }
   }
 
   window.onscroll = function() {
+    var fadedBlockParagraphPositions = [];
     for (var k = 0; k < fadedTextBlocks.length; k++) {
-      var paragraphElementBoundingRect = fadedTextBlocks[k].getBoundingClientRect();
-
-      if ((window.pageYOffset || document.documentElement.scrollTop) + 300 >= paragraphElementBoundingRect.top + (window.pageYOffset || document.documentElement.scrollTop)) {
-        var allViewableSpans = document.querySelectorAll('[class*="evo_h-fade-lines-"]');
-
-        for (var l = 0; l < allViewableSpans.length; l++) {
-            allViewableSpans[l].style.top =  0;
-            // allViewableSpans[l].style.transform = "translateY(0)";
-            allViewableSpans[l].style.opacity =  1;
+      fadedBlockParagraphPositions.push(fadedTextBlocks[k].getBoundingClientRect());
+      if ((window.pageYOffset || document.documentElement.scrollTop) + 300 >= fadedBlockParagraphPositions[k].top + (window.pageYOffset || document.documentElement.scrollTop)) {
+        for (var l = 0; l < fadedTextBlocks[k].childNodes.length; l++) {
+          if (fadedTextBlocks[k].childNodes[l].tagName === "SPAN") {
+            fadedTextBlocks[k].childNodes[l].style.top = 0;
+            // fadedTextBlocks[k].childNodes[l].style.transform = "rotate(0deg)";
+            fadedTextBlocks[k].childNodes[l].style.opacity = 1;
+          }
         }
       }
     }
