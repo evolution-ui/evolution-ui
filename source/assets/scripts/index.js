8f6da382fd75562e30ff49385b7d493247fa3af0
Chris Bracco <chris@cbracco.me>
2017-05-02 10:19:23 -0400

    chore(legacy): Remove references to old example components

--- source/assets/scripts/index.js
+++ source/assets/scripts/index.js
@@ -1,12 +1,8 @@
 import documentReady from './libs/document-ready';
 import audioPlayer from '../../components/standard/audio-player';
-import myOtherComponent from '../../components/standard/my-other-component';
-import myComponent from '../../components/evolution/my-component';
 import curtain from '../../components/evolution/curtain';
 
 documentReady(
   audioPlayer,
-  myComponent,
-  myOtherComponent,
   curtain
 );
