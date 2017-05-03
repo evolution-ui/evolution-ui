fb3b313a5cc1ac64f582520a31823c54f1c9b5e8
Sebastian <sebastian.r.rb@gmail.com>
2017-05-02 23:35:33 -0400

    Remove crumble changes so we can rebase

--- source/assets/scripts/index.js
+++ source/assets/scripts/index.js
@@ -1,10 +1,8 @@
 import documentReady from './libs/document-ready';
 import audioPlayer from '../../components/standard/audio-player';
 import curtain from '../../components/evolution/curtain';
-import crumble from '../../components/evolution/crumble';
 
 documentReady(
   audioPlayer,
-  curtain,
-  crumble
+  curtain
 );
