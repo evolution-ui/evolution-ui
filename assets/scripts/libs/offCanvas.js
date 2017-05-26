832a55d868f4c0c68ef51d63accd6478a1c808bf
Sebastian <sebastian.r.rb@gmail.com>
2017-05-25 20:23:05 -0400

    Update 2017-05-26T00:23:04.756Z

--- assets/scripts/libs/offCanvas.js
+++ assets/scripts/libs/offCanvas.js
@@ -15,8 +15,21 @@ var offCanvas = (function () {
     var toggle = document.querySelector(toggleSelector);
     var target = document.querySelector(targetSelector);
 
+
     toggle.addEventListener('click', function (event) {
       this.classList.toggle('is-active');
+
+      var $selectedLayer = $('.selected-layer');
+      var $unselectedLayer = $('.evo_c_multiLayers_container section').not(".selected-layer");
+
+      if(this.classList.contains('is-active')) {
+        $unselectedLayer.css('pointerEvents', 'none');
+      } else {
+        $unselectedLayer.css('pointerEvents', 'auto');
+      }
+
+      $selectedLayer.toggleClass('overflow-visible');
+
       target.classList.toggle('is-visible');
     });
   }
